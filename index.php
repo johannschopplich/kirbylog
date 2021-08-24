07398ed01b354a73b4cc1124b74f5156ef532182
Johann Schopplich <mail@johannschopplich.com>
2021-08-24 09:31:25 +0200

    refactor: remove duplicate option keys

--- index.php
+++ index.php
@@ -2,10 +2,4 @@
 
 @include_once __DIR__ . '/vendor/autoload.php';
 
-\Kirby\Cms\App::plugin('johannschopplich/kirbylog', [
-    'options' => [
-        'dir' => fn () => kirby()->root('logs'),
-        'filename' => strftime('%Y-%m-%d') . '.log',
-        'defaultLevel' => 'INFO'
-    ]
-]);
+\Kirby\Cms\App::plugin('johannschopplich/kirbylog', []);
